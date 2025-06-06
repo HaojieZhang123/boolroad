34ec5a66c155b69a9bc84475d43352c5aa36e4b7
HaojieZhang123 <haojiezhang123@gmail.com>
2025-06-06 17:14:20 +0200

    [UPD] search bar logic done

--- src/pages/TripDetails.jsx
+++ src/pages/TripDetails.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { use } from 'react'
 import TravelerCard from '../components/TravelerCard'
 import { useArray } from '../contexts/arrayContext'
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const TripDetails = () => {
+
     const { array } = useArray();
 
     const { id } = useParams();
 
     // ricerca del viaggio specifico
     const trip = array.find((trip) => trip.id === parseInt(id));
+
+    const [travelers, setTravelers] = useState(trip.travelers);
+    const [search, setSearch] = useState([]);
+    const [filteredTravelers, setFilteredTravelers] = useState([]);
+
+    // function to search travelers based on name or surname
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const searchedName = e.target.value;
+        const filtered = travelers.filter((traveler) => {
+            // chain name and surname together and convert to lowercase
+            const fullName = `${traveler.name} ${traveler.surname}`.toLowerCase();
+
+            // search for name or surname
+            return fullName.includes(searchedName.toLowerCase());
+        });
+        setFilteredTravelers(filtered);
+        setSearch(searchedName);
+    }
+
+    useEffect(() => {
+        setFilteredTravelers(travelers);
+    }, [travelers]);
 
     return (
         <>
@@ -28,13 +53,10 @@ const TripDetails = () => {
                     <h2>Rubrica dei viaggiatori</h2>
                 </div>
                 <div className="col-12">
-                    <form>
-                        <div className="mb-3">
-                            <label htmlFor="exampleInputEmail1" className="form-label">Inserisci nome</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        </div>
-                        <button type="submit" className="btn btn-primary">Search</button>
-                    </form>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Inserisci nome</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={search} onChange={handleSearch} />
+                    </div>
                 </div>
             </div>
 
@@ -42,7 +64,7 @@ const TripDetails = () => {
             <div className="row my-3">
                 <div className="col-12">
                     <div className="accordion" id="accordionExample">
-                        {trip.travelers.map((traveler) => (
+                        {filteredTravelers.map((traveler) => (
                             <TravelerCard key={traveler.id} traveler={traveler} />
                         ))}
                     </div>
